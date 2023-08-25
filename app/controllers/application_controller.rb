70cb563a8fa9f06e1c64a528f9b9d3d5cfb5647e
Varsion <jianhual12@outlook.com>
2023-08-26 00:31:10 +0800

    fix: error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::API
     chess_row = %w(1 2 3 4 5 6 7 8)
 
     dx = [2, 2, -2, -2, 1, 1, -1, -1]
-    dy = [-1, 1, 1, -1, 2, -2, 2, -2]
+    dy = [-1, 1, -1, 1, -2, 2, -2, 2]
 
     visited = []
     count = 0
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::API
           new_x = chess_col.index(node[0]) + dx[i]
           new_y = chess_row.index(node[1]) + dy[i]
 
-          if (new_x > 0 && new_x < 8) && (new_y > 0 && new_y < 8)
+          if (new_x >= 0 && new_x < 8) && (new_y >= 0 && new_y < 8)
             new_position = chess_col[new_x] + chess_row[new_y]
             queue.unshift(new_position)
             node_steps[new_position] = Node.new(position: new_position, steps: dist + 1, parent: node_steps[node])
